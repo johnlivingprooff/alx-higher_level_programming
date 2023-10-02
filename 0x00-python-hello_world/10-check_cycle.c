6db03af97dfddcba6780b905474981f7bfe210c4
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-02 23:38:39 +0200

    Floyd’s Cycle-Finding Algorithm

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,7 +7,14 @@
  */
 int check_cycle(listint_t *list)
 {
-	if (list == NULL)
-		return (1);
+	listint_t *slow = list, *fast = list;
+	
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
 	return (0);
 }

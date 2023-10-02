a67c7d93c8e8c84fdf02bddea8ea5a22db2b0c2e
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-02 23:04:49 +0200

    Task10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if linked list is a cycle
+ * @list: the list
+ * Return: 1 - if false, 0 - if true
+ */
+int check_cycle(listint_t *list)
+{
+	if (list == NULL)
+		return (1);
+	return (0);
+}

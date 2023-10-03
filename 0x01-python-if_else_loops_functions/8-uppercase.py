fc79e82cd1a3f4cf58dc859dbdaa2520035b8015
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-03 23:25:36 +0200

    Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strU = ""
+    for char in str:
+        i = ord(char)
+        if 97 <= i <= 122:
+            strU += chr(i - 32)
+        else:
+            strU += char
+    print(strU)

1f7af0d7b9a108ca66cfb5fea621dc241b6dfd8d
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-03 22:45:23 +0200

    Okat

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        if j != 9 and i != 8:
+        if j <= 9 and i != 8:
             print('{}{}, '.format(i, j), end='')
         elif j == 9 and i == 8:
             print('{}{}'.format(i, j))

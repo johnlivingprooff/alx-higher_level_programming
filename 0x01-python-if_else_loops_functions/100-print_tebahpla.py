0e52f19c026c739fd7bc0986cfb1c6bc7df747ce
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-04 01:10:45 +0200

    OKay

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+for code in range(122, 64, -1):
+
+    if 97 <= code <= 122:
+        print('{}'.format(chr(code)), end='')
+        code = code - 33
+    
+    if 65 <= code <= 90:
+        print('{}'.format(chr(code)), end='')
+        code = code + 31
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: the head of the list
+ * @number: where the insertion happens
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}

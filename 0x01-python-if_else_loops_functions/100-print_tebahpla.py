381da59d783b0fa4da56bcb2a2429810f207eba7
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-04 01:30:32 +0200

    while is more flexible in py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
-for code in range(122, 64, -1):
+code = 122
+while code >= 65:
 
     if 97 <= code <= 122:
         print('{}'.format(chr(code)), end='')
-        code = code - 33
+
+    code = code - 33
 
     if 65 <= code <= 90:
         print('{}'.format(chr(code)), end='')
-        code = code + 31
+
+    code = code + 31

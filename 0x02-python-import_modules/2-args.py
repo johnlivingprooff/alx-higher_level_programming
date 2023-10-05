048b1f3800511b0fffbf3bb503c3819f1a72ab01
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-05 22:19:21 +0200

    Dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    arg_len = len(sys.argv)
+
+    if arg_len == 1:
+        print('0 arguments.')
+    elif arg_len > 1:
+        print('{} arguments:'.format(arg_len - 1))
+        for i in range(1, arg_len):
+            print('{}: {}'.format(i, sys.argv[i]))

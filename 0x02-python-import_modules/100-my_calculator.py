7b2000e65b2c7ebc55b2a479ede833e33c5d7ee1
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-06 00:26:37 +0200

    all the executables, updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
 
-        print(f"{a} {op} {b} = {result}")
+    print("{} {} {} = {}".format(a, op, b, result))

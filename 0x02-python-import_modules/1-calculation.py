927a2451638dad890288404fd44239e7faa416a2
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-05 21:35:44 +0200

    Update to the main dir

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print('{} + {} = {}'.format(a, b, add(a, b)))
-print('{} + {} = {}'.format(a, b, sub(a, b)))
-print('{} + {} = {}'.format(a, b, mul(a, b)))
-print('{} + {} = {}'.format(a, b, div(a, b)))
+print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+print('{:d} + {:d} = {:d}'.format(a, b, sub(a, b)))
+print('{:d} + {:d} = {:d}'.format(a, b, mul(a, b)))
+print('{:d} + {:d} = {:d}'.format(a, b, div(a, b)))

ce648dcd782911d87be8cb9a78e67f6cdd05b237
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-06 00:18:05 +0200

    Magic Calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a, op, b = sys.argv[1], sys.argv[2], sys.argv[3]
+    a, b = int(a), int(b)
+    result = None
+    match op:
+        case "+":
+            result = add(a, b)
+        case "*":
+            result = mul(a, b)
+        case "-":
+            result = sub(a, b)
+        case "/":
+            result = div(a, b)
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+
+        print(f"{a} {op} {b} = {result}")

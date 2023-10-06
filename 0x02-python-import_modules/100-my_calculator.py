e2f19b7f637ce209aff7587bd1040d1d284c3478
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-06 14:44:25 +0200

    Indentation error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,13 +3,14 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a, op, b = sys.argv[1], sys.argv[2], sys.argv[3]
     a, b = int(a), int(b)
     result = None
+
     match op:
         case "+":
             result = add(a, b)

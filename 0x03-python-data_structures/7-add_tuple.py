344841ac88de1ecbdea77d5aaf6255196b49321b
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-08 00:16:09 +0200

    Removed redundancy

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,15 +2,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 0:
         tuple_a = (0, 0)
-    elif len(tuple_b) == 0:
+    if len(tuple_b) == 0:
         tuple_b = (0, 0)
-    elif len(tuple_a) == 1:
+    if len(tuple_a) == 1:
         tuple_a = (tuple_a[0], 0)
-    elif len(tuple_b) == 1:
+    if len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
-    elif len(tuple_a) > 2 or len(tuple_b) > 2:
-        tuple_a = (tuple_a[0], tuple_a[1])
-        tuple_b = (tuple_b[0], tuple_b[1])
 
     a = tuple_a[0] + tuple_b[0]
     b = tuple_a[1] + tuple_b[1]

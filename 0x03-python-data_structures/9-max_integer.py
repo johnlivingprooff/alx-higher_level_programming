b8f5d638659d8b83d21d33574eebe36b1015e56d
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-07 23:23:17 +0200

    Finding Nemo :)

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    maxi = my_list[0]
+
+    for x in my_list:
+        if x > maxi:
+            maxi = x
+        
+    return maxi

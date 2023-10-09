f3eea29f058a87510b8e200ea6e0e9b74a54c248
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-09 15:40:20 +0200

    Traversing linked-lists

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,7 +7,7 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow, *fast, *prev = NULL, *current = *head, *nex = NULL;
+	listint_t *slow, *fast, *prev = NULL, *nex = NULL;
 	listint_t *firstHalf, *secondHalf;
 
 	if (*head == NULL || (*head)->next == NULL)
@@ -22,25 +22,23 @@ int is_palindrome(listint_t **head)
 		fast = fast->next->next;
 	}
 	/* Reverse the second half of l-list */
-	while (current != NULL && slow != NULL)
+	while (slow != NULL)
 	{
-		nex = current->next;
-		current->next = prev;
-		prev = current;
-		current = nex;
+		nex = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = nex;
 	}
-
 		firstHalf = *head;
 		secondHalf = prev;
 	/* Comparing the first& second halves */
-	while (firstHalf != NULL && secondHalf != NULL)
+	while (secondHalf != NULL)
 	{
 		if (firstHalf->n != secondHalf->n)
 			return (0); /* not a palindrome list */
 		firstHalf = firstHalf->next;
 		secondHalf = secondHalf->next;
 	}
-
 
 	return (1); /* a palindrome list */
 }

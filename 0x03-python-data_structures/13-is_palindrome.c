f2669519ef5a3e4eba3ad86306af7111af4fd947
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-09 02:24:44 +0200

    Adding a check for a4

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,38 +13,37 @@ int is_palindrome(listint_t **head)
 	if (*head == NULL)
 		return (1); /* empty list is palindrome */
 
-	if ((*head)->next != NULL)
-		return (0); /* not a palindrome */
-	else
+	if ((*head)->next == NULL)
+		return (1); /* not a palindrome */
+
+	slow = *head;
+	fast = *head;
+	/* finding the middle of l-list */
+	while (fast != NULL && fast->next != NULL)
 	{
-		slow = *head;
-		fast = *head;
-		/* finding the middle of l-list */
-		while (fast != NULL && fast->next != NULL)
-		{
-			slow = slow->next;
-			fast = fast->next->next;
-		}
-		/* Reverse the second half of l-list */
-		while (current != NULL && slow != NULL)
-		{
-			nex = current->next;
-			current->next = prev;
-			prev = current;
-			current = nex;
-		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	/* Reverse the second half of l-list */
+	while (current != NULL && slow != NULL)
+	{
+		nex = current->next;
+		current->next = prev;
+		prev = current;
+		current = nex;
+	}
 
 		firstHalf = *head;
 		secondHalf = prev;
-		/* Comparing the first& second halves */
-		while (firstHalf != NULL && secondHalf != NULL)
-		{
-			if (firstHalf->n != secondHalf->n)
-				return (0); /* not a palindrome list */
-			firstHalf = firstHalf->next;
-			secondHalf = secondHalf->next;
-		}
+	/* Comparing the first& second halves */
+	while (firstHalf != NULL && secondHalf != NULL)
+	{
+		if (firstHalf->n != secondHalf->n)
+			return (0); /* not a palindrome list */
+		firstHalf = firstHalf->next;
+		secondHalf = secondHalf->next;
 	}
+
 
 	return (1); /* a palindrome list */
 }

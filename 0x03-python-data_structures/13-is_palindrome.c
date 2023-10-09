c24480fc983f612f84b85a6a6987172f9cdf0fd1
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-09 02:03:09 +0200

    is_palindrome(or not?)

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: pointer to the list
+ * Return: 0 if not palindrome, 1 if otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow, *fast, *prev = NULL, *current = *head, *nex = NULL;
+	listint_t *firstHalf, *secondHalf;
+
+	if (*head == NULL)
+		return (1);
+
+	slow = *head;
+	fast = *head;
+	/* finding the middle of l-list */
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	/* Reverse the second half of l-list */
+	while (current != NULL && slow != NULL)
+	{
+		nex = current->next;
+		current->next = prev;
+		prev = current;
+		current = nex;
+	}
+
+	firstHalf = *head;
+	secondHalf = prev;
+	/* Comparing the first& second halves */
+	while (firstHalf != NULL && secondHalf != NULL)
+	{
+		if (firstHalf->n != secondHalf->n)
+			return (0); /* not a palindrome list */
+		firstHalf = firstHalf->next;
+		secondHalf = secondHalf->next;
+	}
+
+	return (1);
+}

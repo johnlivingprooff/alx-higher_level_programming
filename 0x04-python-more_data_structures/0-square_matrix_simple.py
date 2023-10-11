0dffe7c6fff37f7f29646ca24e06804815afdc4c
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-11 20:17:17 +0200

    Corrected syntax errors

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,11 @@
 def square_matrix_simple(matrix=[]):
     # Creates a new matrix, with similar dimensions as matrix
     new_matrix = [
-            [0 for _ in range(len(matrix[]))]
-            for _ in range(len(martix))
+            [0 for _ in range(len(matrix[0]))]
+            for _ in range(len(matrix))
             ]
-    new_matrix = [[x**2 for x in row] for x in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] = matrix[i][j] ** 2
 
     return new_matrix

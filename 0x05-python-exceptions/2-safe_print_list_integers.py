0b6adb7094a94ea47e8e8461683c04672a58537d
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-23 22:43:11 +0200

    Updating for loop for Tsk2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(0, x):
+    count = 0
+    for i in range(0, x):
+        try:
             if isinstance(my_list[i], int):
                     print("{:d}".format(my_list[i]), end='')
                     count += 1
-        print()
-        return count
-    except ValueError:
-        print()
-        return count
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count

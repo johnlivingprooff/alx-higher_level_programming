3dfb1a25acf11be1454dec792c0ee5ccc3b72f17
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-24 13:44:36 +0200

    Adding docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
+"""A group of functions that define a square class"""
 class Square:
-    """This Class represents a square
-    it is defined in ...
-    """
+    """This Class represents a square"""
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""A group of functions that define a square class"""
 class Square:
     """This Class represents a square
     it defines the attributes and methods of a Square ...

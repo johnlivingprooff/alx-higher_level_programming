c9a3ae15c4bcd6c0a49e54207712458eee34df42
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-24 15:37:30 +0200

    fixed indentations

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,6 +25,19 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     # define the area method
     def area(self):
         """returns the area of the square"""

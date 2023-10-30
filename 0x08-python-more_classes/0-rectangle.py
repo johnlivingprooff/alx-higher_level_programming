cce6bf6508b22a7aac950fb546df1db20a289e5b
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-30 19:53:17 +0200

    Update three tasks

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+An Empty class that defines a Rectangle
+"""
+
+
+class Rectangle:
+    """The class represents a Rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+An Empty class that defines a Rectangle
+"""
+
+
+class Rectangle:
+    """The class represents a Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.___height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        
+        self.__height = value

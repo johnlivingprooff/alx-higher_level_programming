b8945df943e9e2c4717297c4e62d069a0a020793
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-30 22:56:57 +0200

    fixed pyc

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -68,14 +68,14 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        
+
         area_1 = rect_1.area()
         area_2 = rect_2.area()
 
@@ -83,7 +83,7 @@ class Rectangle:
             return rect_1
         if area_1 < area_2:
             return rect_2
-        
+
     @classmethod
     def square(cls, size=0):
         return cls(size, size)

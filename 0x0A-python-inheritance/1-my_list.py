b9dfe1b1937cb2edbff0587443d43637a3019141
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-07 05:07:29 +0200

    commentary 1-my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,8 @@ This is a module ``1-my_list``
 
 
 class MyList(list):
+    """class represents a list"""
 
-	def print_sorted(self):
-		print(sorted(self))
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))

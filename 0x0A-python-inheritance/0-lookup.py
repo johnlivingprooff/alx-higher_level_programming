d1c49551b1e92660d182901a0d1a82ebad2f28e9
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-07 05:01:42 +0200

    0-lookup with space indentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """a function that returns the list of available
-	attributes and methods of an object:"""
+    attributes and methods of an object:"""
 
 
 def lookup(obj):
-	return dir(obj)
+    return dir(obj)

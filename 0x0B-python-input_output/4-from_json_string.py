13774fbcc4770a3ed0b620bf914730a2f4b22270
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-08 06:10:23 +0200

    update to task-4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhthon3
+"""function that returns an object (Python data structure)
+represented by a JSON string:"""
+
+
+def from_json_string(my_str):
+    import json
+    obj = json.loads(my_str)
+    return obj

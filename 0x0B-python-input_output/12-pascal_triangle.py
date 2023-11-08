a3b82824ab69892ab08dfe5d4e8af74a09a5a781
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-08 20:45:22 +0200

    pascal-triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,7 +8,7 @@ representing the Pascal’s triangle of n:"""
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
+
     triad = [[1]]
 
     for row in range(1, n):
@@ -18,7 +18,7 @@ def pascal_triangle(n):
         for i in range(1, row):
             element = prev[i - 1] + prev[i]
             new.append(element)
-        
+
         new.append(1)
         triad.append(new)
 

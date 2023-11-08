b415b3dd3c470dc00f862d8bc44236b7585aec12
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-08 07:00:10 +0200

    Update to Tsk0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-function that reads a text file (UTF8) and prints it to stdout"""
+"""function that reads a text file (UTF8)
+and prints it to stdout"""
 
 
 def read_file(filename=""):
     """opens and reads a file"""
     with open(filename, encoding="UTF-8") as f:
-        _print = f.read()
-    print(_print)
-    f.close()
+        file_content = f.read()
+    print(file_content)

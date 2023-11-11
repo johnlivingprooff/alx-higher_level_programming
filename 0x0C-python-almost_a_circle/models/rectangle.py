10d2b0ee63a0e7aee896540de4b5054c3bf7452f
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-11 13:45:53 +0200

    Update 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,29 @@ class Rectangle(Base):
         msg = "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.__x, self.__y, self.__width, self.__height)
         return msg
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        elif kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this module inherits a rectangle class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """this class defines a square
+    based on Rectangle class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        msg = "[Square] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.height)
+        return msg
+    
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width   = value
+        self.height = value

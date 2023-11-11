a5c5303b6ad9167894816b617a14bb2f9c387159
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-11 13:57:43 +0200

    square to_dictionary method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -44,3 +44,8 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        sq_dict = {"id":self.id, "size":self.size,
+                     "x":self.x, "y":self.y}
+        return sq_dict

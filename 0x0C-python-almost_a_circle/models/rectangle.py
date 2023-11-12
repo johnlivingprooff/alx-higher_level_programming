1bde4d48f7c38672cefe503eb984a8363ad40ee3
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-12 16:32:39 +0200

    test_load_save_file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -65,7 +65,7 @@ class Rectangle(Base):
     def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,8 @@
 Project Almost a Circle"""
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -10,16 +12,16 @@ class TestBase(unittest.TestCase):
     def test_create(self):
         obj = Base()
         obj2 = Base(5)
-        obj3 = Base()
+        obj3 = Base(2)
         obj4 = Base(5.8)
         obj5 = Base(None)
         obj6 = Base(-9)
 
-        self.assertEqual(obj.id, 1)
+        self.assertEqual(obj.id, 11)
         self.assertEqual(obj2.id, 5)
         self.assertEqual(obj3.id, 2)
         self.assertEqual(obj4.id, 5.8)
-        self.assertEqual(obj5.id, 3)
+        self.assertEqual(obj5.id, 12)
         self.assertEqual(obj6.id, -9)
 
     def test_to_json_string(self):
@@ -31,6 +33,63 @@ class TestBase(unittest.TestCase):
         j_str = Base.from_json_string(None)
 
         self.assertEqual(j_str, [])
+
+
+class BaseTest_file(unittest.TestCase):
+    """checks file operaion"""
+    def test_save_load_file_rectangle(self):
+        obj = Rectangle(4, 6)
+        obj1 = Rectangle(10, 25)
+        obj2 = Rectangle(15, 4)
+
+        Rectangle.save_to_file([obj, obj1])
+        with open("Rectangle.json", "r") as f:
+            content = f.read()
+            self.assertTrue(len(content), 52)
+        Rectangle.save_to_file(None)
+        with open('Rectangle.json', 'r') as file:
+            content = file.read()
+            self.assertEqual(content, '[]')
+
+        Rectangle.save_to_file([obj1, obj2])
+        instance = Rectangle.load_from_file()
+
+        self.assertEqual(len(instance), 2)
+        self.assertEqual(instance[0].id, 2)
+        self.assertEqual(instance[1].id, 3)
+
+    def test_empty_file_rectangle(self):
+        Rectangle.save_to_file(None)
+        instance = Rectangle.load_from_file()
+
+        self.assertEqual(len(instance), 0)
+
+    def test_save_load_file_square(self):
+        obj = Square(6)
+        obj1 = Square(25)
+        obj2 = Square(15)
+
+        Square.save_to_file([obj, obj1])
+        with open("Square.json", "r") as f:
+            content = f.read()
+            self.assertTrue(len(content), 52)
+        Square.save_to_file(None)
+        with open('Square.json', 'r') as file:
+            content = file.read()
+            self.assertEqual(content, '[]')
+
+        Square.save_to_file([obj1, obj2])
+        instance = Square.load_from_file()
+
+        self.assertEqual(len(instance), 2)
+        self.assertEqual(instance[0].id, 7)
+        self.assertEqual(instance[1].id, 8)
+
+    def test_empty_file_square(self):
+        Square.save_to_file(None)
+        instance = Square.load_from_file()
+
+        self.assertEqual(len(instance), 0)
 
 
 if __name__ == "__main__":

abe9aa00f7026b7aad0157c6fdbc866084318ba2
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-14 15:33:44 +0200

    changed order of update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -119,15 +119,15 @@ class Rectangle(Base):
         """assigns an argument to each attribute"""
         if args:
             if len(args) > 0:
-                self.__width = args[0]
+                self.id = args[0]
             if len(args) > 1:
-                self.__height = args[1]
+                self.__width = args[1]
             if len(args) > 2:
-                self.__x = args[2]
+                self.__height = args[2]
             if len(args) > 3:
-                self.__y = args[3]
+                self.__x = args[3]
             if len(args) > 4:
-                self.id = args[4]
+                self.__y = args[4]
         elif kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, key, value)

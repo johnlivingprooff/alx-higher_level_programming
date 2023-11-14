d76ce5aec212f0d6a9ed783e82261c273167190e
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-14 02:57:42 +0200

    square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,13 +30,13 @@ class Square(Rectangle):
         """assigns an argument to each attribute"""
         if args:
             if len(args) > 0:
-                self.size = args[0]
+                self.id = args[0]
             if len(args) > 1:
-                self.x = args[1]
+                self.size = args[1]
             if len(args) > 2:
-                self.y = args[2]
+                self.x = args[2]
             if len(args) > 3:
-                self.id = args[3]
+                self.y = args[3]
         elif kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, key, value)

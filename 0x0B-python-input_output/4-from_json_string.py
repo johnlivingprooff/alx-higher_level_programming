dfde296a740bea2704b6e09422027a7835808d41
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-12 05:08:14 +0200

    adjusted requirements

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """function that returns an object (Python data structure)
 represented by a JSON string:"""
+import json
 
 
 def from_json_string(my_str):
-    import json
     obj = json.loads(my_str)
+    # for k, v in obj.items():
+    #     obj[k] = v
     return obj

29b461f223938c845f7170d590d646d2044c76ea
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 22:03:35 +0200

    group by different

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, SUM(tv_shows_rate.rating) AS rating_sum
 FROM tv_shows
 INNER JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
-GROUP BY tv_shows.title
+GROUP BY tv_shows_rate.show_id
 ORDER BY rating_sum DESC;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,7 +2,6 @@
 SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
-GROUP BY tv_genres.name
+GROUP BY tv_show_genres.genre_id
 ORDER BY rating_sum DESC;

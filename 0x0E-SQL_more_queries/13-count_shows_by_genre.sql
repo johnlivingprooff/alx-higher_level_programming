a1137221c34838fd829c6bd75f98c02e4b609be8
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 21:42:13 +0200

    [A

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List genres with the number of shows linked
-SELECT genre_id,
-       COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_show_genres.genre_id AS 'genre',
+       COUNT(tv_show_genres.show_id) AS 'number_of_shows'
 FROM tv_show_genres
 GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+ORDER BY number_of_shows DESC;  

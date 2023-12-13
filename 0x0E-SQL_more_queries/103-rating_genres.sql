f5164f47887a68c79768dac1dadfd505fa47a252
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 22:00:46 +0200

    inner joins

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,8 @@
 -- List genres in hbtn_0d_tvshows_rate by their rating
 SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
 GROUP BY tv_genres.name
 ORDER BY rating_sum DESC;

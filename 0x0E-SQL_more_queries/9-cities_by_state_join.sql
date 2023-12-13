f8d10c0914c7f72a2fab6c5bcf12dba934d09540
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 21:46:26 +0200

    state name"

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 -- List all cities with corresponding state names
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name AS 'state_name'
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;

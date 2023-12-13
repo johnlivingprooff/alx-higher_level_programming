a44387142e3af41fc992ac1b66bb489427029f5d
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 21:43:31 +0200

    updated indentation

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- List genres with the number of shows linked
-SELECT tv_show_genres.genre_id AS 'genre',
-       COUNT(tv_show_genres.show_id) AS 'number_of_shows'
-FROM tv_show_genres
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;  
+SELECT tv_genres.name AS 'genre',
+    COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;

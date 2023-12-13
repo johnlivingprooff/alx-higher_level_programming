2c08bb8e732e85c75d9186855edaf8a7a2e0e9f8
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 21:45:45 +0200

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa.
-
-USE hbtn_0d_usa;
-
 -- List all cities with corresponding state names
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities, states

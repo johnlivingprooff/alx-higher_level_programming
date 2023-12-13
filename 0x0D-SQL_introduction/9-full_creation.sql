ab5fccb0f8142d256f902c511b645d10c1bafe86
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 20:41:56 +0200

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 
 -- Add multiple rows to the second_table
 
-INSERT INTO ', @db_NAME, '.', @table_NAME, ' (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES
     (1, "John", 10),
     (2, "Alex", 3),

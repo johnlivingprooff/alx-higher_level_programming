79a8cd32653109aa952d7fcdfa28e3d89b9fa1d6
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 20:46:42 +0200

    removed a comment

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -2,7 +2,6 @@
 -- of the database hbtn_0c_0 in your MySQL server.
 
 -- List all records of the second_table ordered by score
--- Construct the query and execute it
 
 SELECT score, name
 FROM second_table

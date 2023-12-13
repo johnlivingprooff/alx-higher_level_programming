ed96a59220d48e42e7a559417104e3b5b7a5ee42
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-13 21:38:27 +0200

    not AS genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- List all shows with linked genres
-SELECT title, name AS genre
+SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id

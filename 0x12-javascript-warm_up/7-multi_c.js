38d27d737af4532178b8e6e34bea05abe9b28c92
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-08 19:19:46 +0200

    isn't C fun?

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const myStr = 'C is fun';
+const arg = process.argv.slice(2);
+let i = 0;
+
+if (!isNaN(parseInt(arg[0]))) {
+    while (i < parseInt(arg)) {
+        console.log(myStr);
+        i++;
+    }
+} else {
+    console.log('Missing number of occurrences')
+}

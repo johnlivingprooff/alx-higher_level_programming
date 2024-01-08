db725394fd454b232795dfb07d789c218a3bca70
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-08 16:35:57 +0200

    js uses 2-spaces indentation

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const args = process.argv.length - 1;
 
 if (args <= 1) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (args === 2) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }

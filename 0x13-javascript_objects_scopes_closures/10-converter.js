bb8c7d6e3792a7aeb18342aa6a543648dab982dd
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-09 23:17:53 +0200

    jkl

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (number) {
-    if (number > 0) {
-      exports.converter(Math.floor(number / base));
-      return (String((number % base)).toString());
-    }
+    return number.toString(base);
   };
 };

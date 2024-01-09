9444392f6b17b7284728fa3c3f32755979f71787
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-09 11:02:34 +0200

    class Rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        return;
-    } else {
-      this.height = h;
+    if (Number.isInteger(w) && w > 0 && h > 0 && Number.isInteger(h)) {
       this.width = w;
+      this.height = h;
     }
   }
 }

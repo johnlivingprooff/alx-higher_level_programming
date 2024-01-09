cd6bf51d5fa50b5cf1cb3c9998965a48a85f95aa
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-09 23:00:16 +0200

    .

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,12 +3,12 @@ const dict = require('./101-data').dict;
 const myDict = {};
 
 for (const uID in dict) {
-    const occurances = dict[uID];
-    if (occurances in myDict) {
-        myDict[occurances].push(uID);
-    } else {
-        myDict[occurances] = [uID];
-    }
+  const occurances = dict[uID];
+  if (occurances in myDict) {
+    myDict[occurances].push(uID);
+  } else {
+    myDict[occurances] = [uID];
+  }
 }
 
 console.log(myDict);

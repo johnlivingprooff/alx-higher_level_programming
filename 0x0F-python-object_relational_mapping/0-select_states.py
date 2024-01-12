9781d0de4539127d7f1540cdf84d8f3628db5cfe
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-12 16:40:08 +0200

    rest of mind

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa:"""
 import MySQLdb
 import sys
-"""lists all states from the database hbtn_0e_0_usa:"""
 
 
 def lister(user_n, passw, database):

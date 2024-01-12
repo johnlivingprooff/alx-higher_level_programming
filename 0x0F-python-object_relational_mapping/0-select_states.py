0bf351f251978d53bcb2a81fb1a20b53acde31fa
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-12 15:51:14 +0200

    removed function

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,25 +4,24 @@ import sys
 """lists all states from the database hbtn_0e_0_usa:"""
 
 
-def lister(user, passw, database):
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=user, passwd=passw, db=database
-        )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         sys.exit(1)
 
     user, passw, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    query = """SELECT * FROM states ORDER BY states.id ASC"""
+    dbase = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=user, passwd=passw,
+        db=database, charset="utf8"
+    )
+    mouse = dbase.cursor()
+    mouse.execute(query)
+    rows = mouse.fetchall()
+    for row in rows:
+        print(row)
+
+    mouse.close()
+    dbase.close()
+
     # print(f"User: {user}, Password: {passw}, Database: {database}")
-    lister(user, passw, database)

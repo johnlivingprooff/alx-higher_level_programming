10469fcc53a4a0b0198457e5b1186817e2dfea4f
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-12 16:16:10 +0200

    okay

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,21 +4,29 @@ import sys
 """lists all states from the database hbtn_0e_0_usa:"""
 
 
-if __name__ == "__main__":
-    user, passw, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    query = """SELECT * FROM states ORDER BY id ASC;"""
+def lister(user_n, passw, database):
+    """this function does all the querying"""
+
+    query = "SELECT * FROM states ORDER BY id ASC;"
     dbase = MySQLdb.connect(
-        host="localhost",
-        port=3306, user=user, passwd=passw,
-        db=database, charset="utf8"
+        host="localhost", port=3306, user=user_n, passwd=passw, db=database
     )
     mouse = dbase.cursor()
     mouse.execute(query)
     rows = mouse.fetchall()
+
     for row in rows:
         print(row)
 
     mouse.close()
     dbase.close()
 
-    # print(f"User: {user}, Password: {passw}, Database: {database}")
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    user_n = sys.argv[1]
+    passw = sys.argv[2]
+    database = sys.argv[3]
+
+    lister(user_n, passw, database)

0a494e146e54e4ce2a3f006753c5a2d896a1a2ed
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-12 15:52:42 +0200

    gg

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        sys.exit(1)
+    # if len(sys.argv) != 4:
+    #     sys.exit(1)
 
     user, passw, database = sys.argv[1], sys.argv[2], sys.argv[3]
     query = """SELECT * FROM states ORDER BY states.id ASC"""

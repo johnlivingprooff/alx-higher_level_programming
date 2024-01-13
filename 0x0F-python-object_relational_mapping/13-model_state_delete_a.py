ab077f2914ace39964973f1acda7bcf96464f872
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-13 16:11:44 +0200

    update

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+with sqlalchemy
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def core(user, passw, database):
+    """the essential function
+    for the operation"""
+    dbase_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.\
+                format(user, passw, database)
+    engine = create_engine(dbase_uri, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    sesh = Session()
+
+    sesh.query(State).filter(State.name.like('%a%')).delete()
+    sesh.commit()
+    sesh.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        core(sys.argv[1], sys.argv[2], sys.argv[3])

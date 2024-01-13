fda6a332919485779ef5353b4a00acff4be80cf6
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-13 19:04:53 +0200

    update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,29 +5,24 @@
 """
 from relationship_city import City
 from relationship_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-import sys
-
-
-def core(user, passw, database):
-    """the essential function
-    for the operation"""
-    dbase_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.\
-                format(user, passw, database)
-    engine = create_engine(dbase_uri, pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
-    sesh = Session()
-
-    for state in sesh.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
-
-    sesh.close()
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        core(sys.argv[1], sys.argv[2], sys.argv[3])
+    if len(argv) == 4:
+        dbase_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.\
+                    format(argv[1], argv[2], argv[3])
+        engine = create_engine(dbase_uri, pool_pre_ping=True)
+
+        Session = sessionmaker(bind=engine)
+        sesh = Session()
+
+        for state in sesh.query(State).order_by(State.id).all():
+            print("{}: {}".format(state.id, state.name))
+            for city in state.cities:
+                print("\t{}: {}".format(city.id, city.name))
+
+        sesh.close()
+            

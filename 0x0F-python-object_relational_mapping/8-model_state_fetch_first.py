fafca4293eaf5e75f6794e6b8bc478af588fcf35
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-13 15:13:06 +0200

    .first()

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,8 +4,7 @@ lists all State objects from the database hbtn_0e_6_usa
 with sqlalchemy; only the first state
 """
 import sys
-from model_state import Base
-from model_state import State
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -20,9 +19,9 @@ def core(user, passw, database):
     Session = sessionmaker(bind=engine)
     sesh = Session()
 
-    states = sesh.query(State).order_by(State.id).all()
+    state = sesh.query(State).order_by(State.id).first()
     # for state in states:
-    print(f"{states[0].id}: {states[0].name}")
+    # print(f"{states[0].id}: {states[0].name}")
 
     sesh.close()
 

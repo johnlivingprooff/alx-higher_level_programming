0f3a6c2dee5ee988d24c528ee9028e56573e20ad
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-26 22:55:52 +0200

    update README.md

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,12 @@ if __name__ == "__main__":
     reques = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(reques) as response:
         the_page = response.read()
+        encoder = the_page.decode('utf-8')
 
         print('Body Response:')
-        print(f"\t- type: {type(the_page)}")
-        print(f"\t- content: {the_page}")
-        print(f"\t- utf8 content: {the_page.decode('utf-8')}")
+        # print(f"\t- type: {type(the_page)}")
+        print("    - type: {}".format(type(the_page)))
+        # print(f"\t- content: {the_page}")
+        print("    - content: {}".format(the_page))
+        # print(f"\t- utf8 content: {the_page.decode('utf-8')}")
+        print("    - utf8 content: {}".format(encoder))

42478138d726f362d8ba98822661cd73a40c80b8
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-27 09:47:42 +0200

    6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = { "email": argv[2] }
+    response = requests.post(argv[1], data)
+
+    print(response.text)

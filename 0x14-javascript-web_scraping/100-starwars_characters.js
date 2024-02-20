192f6d3db5fbd47cd5d1832c349c584f505813f5
johnlivingprooff <johnlivingprooff@gmail.com>
2024-02-20 19:42:27 +0200

    correction 100.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -9,34 +9,16 @@ request.get(url, (error, response, body) => {
     console.error(error);
   } else {
     const data = JSON.parse(body);
-    const characters = data.characters;
-    const promises = [];
 
-    characters.forEach(characterUrl => {
-      // Create a promise for each character request
-      const promise = new Promise((resolve, reject) => {
-        request.get(characterUrl, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            const characterData = JSON.parse(body);
-            resolve(characterData.name);
-          }
-        });
+    data.characters.forEach(character => {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const charName = JSON.parse(body);
+          console.log(charName.name);
+        }
       });
-      promises.push(promise);
     });
-
-    // Wait for all promises to resolve
-    Promise.all(promises)
-      .then(characterNames => {
-        // Print character names in order
-        characterNames.forEach(name => {
-          console.log(name);
-        });
-      })
-      .catch(error => {
-        console.error(error);
-      });
   }
 });

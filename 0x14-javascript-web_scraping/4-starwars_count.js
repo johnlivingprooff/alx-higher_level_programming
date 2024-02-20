b80a2170de997e18b34a31a712654bff1002d037
johnlivingprooff <johnlivingprooff@gmail.com>
2024-02-20 17:39:30 +0200

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    let n = 0;
+
+    data.results.forEach((film) => {
+      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18')) {
+        n++;
+      }
+    });
+    console.log(n);
+  }
+});

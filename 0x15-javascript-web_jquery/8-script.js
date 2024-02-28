b56d3335d0999ceab8cca4c8a775175ffa4be574
johnlivingprooff <johnlivingprooff@gmail.com>
2024-02-28 20:18:57 +0200

    8. Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+fetch('https://swapi-api.alx-tools.com/api/films/?format=json')
+  .then(resp => {
+    return resp.json();
+  })
+  .then(movies => {
+    movies.results.forEach(movie => {
+      $('#list_movies').append(`<li>${movie.title}</li>`);
+    });
+  });
